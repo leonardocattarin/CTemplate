650a10587601c38fc0ccf014877d0fd764ee9926
Leonardo <leonardo.cattarin@gmail.com>
2022-03-29 12:17:13 +0200

    Test 2

---
+++ lib/Main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+//Sample Function
+int add(int a, int b)
+{
+    return a+b;
+}
+
+//Entry point
+int main(int argc, char *argv[])
+{
+    printf("Number: %d", add(1,2));
+}
